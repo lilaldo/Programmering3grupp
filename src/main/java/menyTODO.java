f04458c3ad53e7b00ebfb91c4af400d9ac55206e
André Aldosson <andre.aldosson@hotmail.se>
2024-01-16 14:39:14 +0100

    Ändrade till en arraylist för att lagra TODO.

--- src/main/java/menyTODO.java
+++ src/main/java/menyTODO.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class menyTODO {
+
+    // Array för att kunna lagra todos.
+    private static ArrayList<String> ärenden = new ArrayList<>();
 
     public static void main(String[] args) {
         String username = loginTODO.login(); // Hämtar användarnamnet från login(hoppas jag)
@@ -15,14 +19,8 @@ public class menyTODO {
     public static void menyalternativ() {
         Scanner scan = new Scanner(System.in);
 
-        // test-array.
-        String[] ärenden = {"TODO now", "TODO later ", "TODO tomorrow "};
-
         while (true) {
-            System.out.println("Dina TODOs:");
-            for (int i = 0; i < ärenden.length; i++) {
-                System.out.println((i + 1) + ". " + ärenden[i]);
-            }
+            visaÄrenden();
 
             System.out.print("Vad vill du göra idag? ");
             String val = scan.nextLine();
@@ -30,7 +28,7 @@ public class menyTODO {
             if (valtÄrende(val, ärenden)) {
                 visaÄrendeInfo(val);
             } else if (val.equals("add")) {     // lägg till
-                läggTillÄrende();
+                läggTillÄrende(scan);
             } else if (val.equals("del")) {     // ta bort
                 taBortÄrende();
             } else if (val.equals("change")) {  // ändra
@@ -43,11 +41,18 @@ public class menyTODO {
         }
     }
 
+    public static void visaÄrenden() {
+        System.out.println("Dina TODOs:");
+        for (int i = 0; i < ärenden.size(); i++) {
+            System.out.println((i + 1) + ". " + ärenden.get(i));
+        }
+    }
+
     // För att fkn java ska fatta vilket jävla nummer jag väljer.
-    public static boolean valtÄrende(String val, String[] ärenden) {
+    public static boolean valtÄrende(String val, ArrayList<String> ärenden) {
         try {
             int index = Integer.parseInt(val) - 1;
-            return index >= 0 && index < ärenden.length;
+            return index >= 0 && index < ärenden.size();
         } catch (NumberFormatException e) {
             return false;
         }
@@ -55,18 +60,26 @@ public class menyTODO {
 
     public static void visaÄrendeInfo(String val) {
         // visa information ärende.
-        System.out.println("Ärende: " + val);
-        // kod för att visa information
+        System.out.println("Ärende: " + ärenden.get(Integer.parseInt(val) - 1));
+        // kod för att visa information ...........
+        // Namn
+        // Information om ärendet?
+        // Prio?
     }
 
-    public static void läggTillÄrende() {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Lägg till nytt ärende:");
-        System.out.print("Ange namn: ");
+    public static void läggTillÄrende(Scanner scan) {
+        System.out.println("Lägg till nytt ärende: ");
         String nyttÄrende = scan.nextLine();
-        // kod för att spara det nya ärendet
-        // Behöver:
-        // Namn, Prio, Datum(?),
+
+        // Kod för att lägga till prioritet och datum.
+        System.out.print("Prioritet: ");
+        String prioritet = scan.nextLine();
+        System.out.print("När? ");
+        String datum = scan.nextLine();
+
+        // Lägg till nya todos med add. behöver justeras.
+        ärenden.add(nyttÄrende + ", Prioritet: " + prioritet + ", När: " + datum);
+
         System.out.println("Ärende '" + nyttÄrende + "' har lagts till.");
     }
 
@@ -74,7 +87,7 @@ public class menyTODO {
         Scanner scan = new Scanner(System.in);
         System.out.println("Vilket ärende vill du ta bort? Ange numret:");
         int index = scan.nextInt() - 1;
-        // kod för att ta bort
+        ärenden.remove(index); // kod för att ta bort ärende från listan.
         System.out.println("Ärende " + (index + 1) + " har tagits bort.");
     }
 
@@ -82,7 +95,7 @@ public class menyTODO {
         Scanner scan = new Scanner(System.in);
         System.out.println("Vilket ärende vill du ändra? Ange numret:");
         int index = scan.nextInt() - 1;
-        // kod för att ändra
+        // kod för att ändra, behöver utvecklas.
         System.out.println("Ärende " + (index + 1) + " har ändrats.");
     }
 }
